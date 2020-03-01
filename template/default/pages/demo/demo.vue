3491babf368674d37144a213c1aea1f2adc5ad12
idawn <hzy@v0718.com>
2020-03-01 08:06:04 +0800

    新增上传到自建服务器的demo

---
+++ template/default/pages/demo/demo.vue
@@ -0,0 +1,42 @@
+<template>
+  <view>
+    <text>上传图片到自己的服务器测试</text>
+    <button @click="uploadToServer"></button>
+  </view>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    uploadToServer() {
+      uni.chooseImage({
+        success: chooseImageRes => {
+          const tempFilePaths = chooseImageRes.tempFilePaths;
+          const uploadTask = uni.uploadFile({
+            url: ApiUrl + "Post.Post", //仅为示例，非真实的接口地址
+            filePath: tempFilePaths[0],
+            name: "file",
+            formData: {
+              uid: 59820,
+              why: why,
+              keyid: keyid
+            },
+            success: uploadFileRes => {
+              console.log(uploadFileRes.data);
+            }
+          });
+          uploadTask.onProgressUpdate(res => {
+            console.log("上传进度" + res.progress);
+            console.log("已经上传的数据长度" + res.totalBytesSent);
+            console.log(
+              "预期需要上传的数据总长度" + res.totalBytesExpectedToSend
+            );
+          });
+        }
+      });
+    }
+  }
+};
+</script>
