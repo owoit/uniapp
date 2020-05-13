bde7ab25446105cfd6c05ca8db1761ad9ee3d73d
idawn <hzy@v0718.com>
2020-05-13 17:08:45 +0800

    新增微信jssdk模块

--- generator.js
+++ generator.js
@@ -20,6 +20,9 @@ async function generate (dir, files, base = '', rootOptions = {}) {
       if (path.basename(filename) === 'manifest.json') {
         content = content.replace('{{name}}', rootOptions.projectName || '')
       }
+      if (path.basename(filename) === 'manifest.json') {
+        content = content.replace('{{name}}', rootOptions.projectName || '')
+      }
       if (filename.charAt(0) === '_' && filename.charAt(1) !== '_') {
         files[`.${filename.slice(1)}`] = content
       } else if (filename.charAt(0) === '_' && filename.charAt(1) === '_') {
@@ -36,7 +39,8 @@ module.exports = (api, options, rootOptions) => {
     return {
       dependencies: {
         'regenerator-runtime': '^0.12.1',// 锁定版本，避免高版本在小程序中出错
-        '@dcloudio/uni-helper-json': '*'
+        '@dcloudio/uni-helper-json': '*',
+        'jweixin-module': '*'
       },
       devDependencies: {
         'postcss-comment': '^2.0.0'
--- template/default/App.vue
+++ template/default/App.vue
@@ -1,8 +1,30 @@
 <script>
-var jweixin = require('jweixin-module')  
-jweixin.ready(function(){  
-    // TODO  
+// ifdef H5
+var jweixin = require("jweixin-module");
+jweixin.config(wxConfig);
+jweixin.ready(function() {
+  jweixin.updateAppMessageShareData(shrData); //群、好友分享信息
+  jweixin.updateTimelineShareData(shrData); //朋友圈分享信息：在需要改分享标题之类时可修改shrData中的值并重先注册本方法
+  // wx.onMenuShareTimeline(shrData); //旧接口
+  // wx.onMenuShareAppMessage(shrData);//旧j
+  jweixin.hideMenuItems({
+    menuList: [
+      "menuItem:share:qq",
+      "menuItem:share:weiboApp",
+      "menuItem:share:facebook",
+      "menuItem:share:QZone",
+      "menuItem:originPage",
+      "menuItem:readMode",
+      "menuItem:openWithQQBrowser",
+      "menuItem:openWithSafari",
+      "menuItem:share:email",
+      "menuItem:share:brand",
+      "menuItem:copyUrl"
+    ]
+  });
 });
+// endif
+
 	export default {
 		onLaunch: function() {
 			console.log('App Launch')
