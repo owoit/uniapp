15108482ee2e0523ce06db20486a4bd5b7a5fe52
iDawn <hzy@v0718.com>
2021-02-24 17:58:34 +0800

    工具(规范): 工具: cz规范

--- .cz-config.js
+++ .cz-config.js
@@ -2,6 +2,7 @@ module.exports = {
     types: [
         {value: '特性',     name: '特性:    一个新的特性'},
         {value: '修复',      name: '修复:    修复一个Bug'},
+        {value: '官方更新',      name: '官方更新:    合并uni-app的官方更新'},
         {value: '文档',     name: '文档:    变更的只有文档'},
         {value: '格式',    name: '格式:    空格, 分号等格式修复'},
         {value: '重构', name: '重构:    代码重构，注意和特性、修复区分开'},
@@ -10,7 +11,7 @@ module.exports = {
         {value: '工具',    name: '工具:    开发工具变动(构建、脚手架工具等)'},
         {value: '回滚',   name: '回滚:    代码回退'}
     ],
-    scopes: [{ name: '分享助力逻辑' }, { name: '活动流程' }, { name: '测试' },{name:'客户要改'},{name:'图片优化'}],
+    scopes: [{ name: '合并官方' }, { name: '新增组件' }, { name: '规范' },{name:'框架修改'},{name:'其他优化'}],
     allowTicketNumber: false,
     isTicketNumberRequired: false,
     ticketNumberPrefix: 'TICKET-',
