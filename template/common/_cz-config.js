7f7330a6ad423ad6a7725d24ebb212ca5f4bc4f4
iDawn <hzy@v0718.com>
2021-02-25 09:14:19 +0800

    docs(规范): gitlog规范

--- template/common/_cz-config.js
+++ template/common/_cz-config.js
@@ -1,14 +1,17 @@
 module.exports = {
     types: [
-        {value: '特性',     name: '特性:    一个新的特性'},
-        {value: '修复',      name: '修复:    修复一个Bug'},
-        {value: '文档',     name: '文档:    变更的只有文档'},
-        {value: '格式',    name: '格式:    空格, 分号等格式修复'},
-        {value: '重构', name: '重构:    代码重构，注意和特性、修复区分开'},
-        {value: '性能',     name: '性能:    提升性能'},
-        {value: '测试',     name: '测试:    添加一个测试'},
-        {value: '工具',    name: '工具:    开发工具变动(构建、脚手架工具等)'},
-        {value: '回滚',   name: '回滚:    代码回退'}
+      {value: 'feat',     name: '特性:    一个新的特性或功能'},
+      {value: 'fix',      name: '修复:    修复一个Bug'},
+      {value: 'uniapp',      name: '官方更新:    合并uni-app的官方更新'},
+      {value: 'docs',     name: '文档:    变更的只有文档'},
+      {value: 'style',    name: '格式:    空格, 分号等格式修复'},
+      {value: 'refactor', name: '重构:    代码重构，注意和特性、修复区分开'},
+      {value: 'perf',     name: '性能:    提升性能'},
+      {value: 'test',     name: '测试:    添加一个测试'},
+      {value: 'build',    name: '工具:    开发工具变动(构建、脚手架工具等)'},
+      {value: 'revert',   name: '回滚:    代码回退'},
+      {value: 'chore',   name: '其他更新:    其他无需记录到log的更新'},
+      {value: 'ci',   name: 'CI/CD:   自动化集成部署相关更新'},
     ],
     scopes: [{ name: '分享助力逻辑' }, { name: '活动流程' }, { name: '测试' },{name:'客户要改'},{name:'图片优化'}],
 
